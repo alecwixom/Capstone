06550777b9e5198741b42bcfa1a1f1932789db93
Alec Wixom <atwixom@gmail.com>
2023-05-24 14:29:51 -0600

    Updated all code to fix GET / error

--- Public/main.js
+++ Public/main.js
@@ -1,77 +1,69 @@
+const messageContainer = document.querySelector('#message-container');
+const form = document.querySelector('form');
+const article = document.querySelector('#aRules');
 
-const messageContainer = document.querySelector('#message-container')
-const form = document.querySelector('form')
-const article = document.querySelector('#aRules')
+const baseURL = 'http://localhost:4001/api/messages';
 
-const baseURL = 'http://localhost:4001/api/messages'
-
-const messagesCallback = ({ data: messages }) => displayMessages(messages)
+const messagesCallback = ({ data: messages }) => displayMessages(messages);
 const errCallback = err => {
     if (err.response && err.response.data) {
-    console.log(err.response.data);
+        console.log(err.response.data);
     } else {
-    console.log(err);
+        console.log(err);
     }
-}
+};
 
-
-const getAllMessages = () => axios.get(baseURL).then(messagesCallback).catch(errCallback)
-const createMsg = body => axios.post(baseURL, body).then(messagesCallback).catch(errCallback)
-const deleteMsg = id => axios.delete(`${baseURL}/${id}`).then(messagesCallback).catch(errCallback)
-
-
+const getAllMessages = () => axios.get(baseURL).then(messagesCallback).catch(errCallback);
+const createMsg = body => axios.post(baseURL, body).then(messagesCallback).catch(errCallback);
+const deleteMsg = id => axios.delete(`${baseURL}/${id}`).then(messagesCallback).catch(errCallback);
 
 function submitHandler(e) {
     e.preventDefault();
 
-    let name = document.querySelector('#firstName')
-    let message = document.querySelector('#message_text')
+    let name = document.querySelector('#firstName').value;
+    let message = document.querySelector('#message_text').value;
 
+    if (!name || !message) {
+        return;
+    }
 
     let bodyObj = {
-        name: name.value,
-        message: message.value
-    }
+        name: name,
+        message: message
+    };
 
     createMsg(bodyObj);
 
-    name.value = '';
-    message.value = '';
-    }
-
-
-
-
-
-
+    document.querySelector('#firstName').value = '';
+    document.querySelector('#message_text').value = '';
+}
 
 function createMessageCard(messager) {
-    const messageCard = document.createElement('div')
-    messageCard.classList.add('message-card')
+    const messageCard = document.createElement('div');
+    messageCard.classList.add('message-card');
 
     messageCard.innerHTML = `
-    <p class= "message-name">${messager.name}</p>
+    <p class="message-name">${messager.name}</p>
     <div class="btns-container">
         <p class="message-body">${messager.message}</p>
     </div>
     <button onclick="deleteMsg(${messager.id})">DELETE</button>
-    `
-    messageContainer.appendChild(messageCard)
+    `;
+
+    messageContainer.appendChild(messageCard);
 }
 
-
-
 function displayMessages(arr) {
-    messageContainer.innerHTML = ``
+    messageContainer.innerHTML = '';
     for (let i = 0; i < arr.length; i++) {
-        createMessageCard(arr[i])
+        createMessageCard(arr[i]);
     }
 }
 
 function dismiss() {
-    article.innerHTML = ``
+    article.innerHTML = '';
 }
 
-form.addEventListener('submit', submitHandler)
+form.addEventListener('submit', submitHandler);
 
-getAllMessages()
+getAllMessages();
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 4001;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+const { getMessages, addMessage, deleteMsg } = require('./controller.js');
 
-const { getMessages, addMessage, deleteMsg } = require('./controller.js')
+app.get('/api/messages', getMessages);
+app.post('/api/messages', addMessage);
+app.delete('/api/messages/:id', deleteMsg);
 
-
-app.get('/api/messages', getMessages)
-app.post('/api/messages', addMessage)
-app.delete('/api/messages/:id', deleteMsg)
-
-
-app.listen(4001, () => {
-    console.log('Were up on 4001!');
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
