6f806c1a7879cafee5c72774b894168b1904d062
Alec Wixom <atwixom@gmail.com>
2023-05-25 09:30:27 -0600

    Adding an auto update method to ther server for messages

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ app.use(express.json());
 app.use(express.static(__dirname + '/Public'))
 
 const { getMessages, addMessage, deleteMsg } = require('./controller.js');
+const db = require ('./db.json')
 
 app.get('/api/messages', getMessages);
 app.post('/api/messages', addMessage);
 app.delete('/api/messages/:id', deleteMsg);
+
+if (db.length === db.length + 1) {
+    getMessages()
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
