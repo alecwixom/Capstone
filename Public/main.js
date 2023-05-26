051528c570f040705beb7497a25c987a806f80ef
Alec Wixom <atwixom@gmail.com>
2023-05-26 13:14:00 -0600

    Fixed the issues with aws, timezone is off but works

--- Public/main.js
+++ Public/main.js
@@ -2,8 +2,8 @@ const messageContainer = document.querySelector('#message-container')
 const form = document.querySelector('form')
 const article = document.querySelector('#aRules')
 
-const baseURL = 'http://54.183.224.121:4001/api/messages'
-// const baseURL = 'http://localhost:4001/api/messages'
+// const baseURL = 'http://54.183.224.121:4001/api/messages'
+const baseURL = 'http://localhost:4001/api/messages'
 
 const messagesCallback = ({ data: messages }) => displayMessages(messages)
 const errCallback = err => {
